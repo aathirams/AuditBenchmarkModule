c484798ef1673fc1f8c038da9d0bb9046e277a99
aathirams <aathisrikumar@outlook.com>
2021-10-05 14:07:03 +0530

    Add project files.

---
+++ Controllers/AuditBenchmarkController.cs
@@ -0,0 +1,44 @@
+﻿using AuditBenchmarkModule.Models;
+using AuditBenchmarkModule.Providers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuditBenchmarkModule.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AuditBenchmarkController : ControllerBase
+    {
+        private readonly log4net.ILog log4netval;
+        private IBenchmarkService objService;
+        public AuditBenchmarkController(IBenchmarkService objService)
+        {
+            log4netval = log4net.LogManager.GetLogger(typeof(AuditBenchmarkController));
+            this.objService = objService;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAuditBenchmark()
+        {
+            List<AuditBenchmark> listOfBenchmark = new List<AuditBenchmark>();
+            log4netval.Info(" Http GET request " + nameof(AuditBenchmarkController));
+            try
+            {
+                listOfBenchmark = objService.GetBenchmark();
+                return Ok(listOfBenchmark);
+            }
+            catch (Exception e)
+            {
+                log4netval.Error(" Exception here" + e.Message + " " + nameof(AuditBenchmarkController));
+                return StatusCode(500);
+            }
+        }
+    }
+}
+
---
+++ Models/AuditBenchmark.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuditBenchmarkModule.Models
+{
+    public class AuditBenchmark { 
+    public string AuditType { get; set; }
+    public int BenchmarkNoAnswers { get; set; }
+    // override object.Equals
+    public override bool Equals(Object obj)
+    {
+        //       
+        // See the full list of guidelines at
+        //   http://go.microsoft.com/fwlink/?LinkID=85237  
+        // and also the guidance for operator== at
+        //   http://go.microsoft.com/fwlink/?LinkId=85238
+        //
+
+        AuditBenchmark benchmark = obj as AuditBenchmark;
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        // TODO: write your implementation of Equals() here
+        if (this.AuditType == benchmark.AuditType && this.BenchmarkNoAnswers == benchmark.BenchmarkNoAnswers)
+            return true;
+        else
+            return false;
+    }
+
+    // override object.GetHashCode
+    public override int GetHashCode()
+    {
+        // TODO: write your implementation of GetHashCode() here
+        return base.GetHashCode();
+    }
+}
+}
